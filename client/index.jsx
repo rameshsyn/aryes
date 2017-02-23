424acdb554f01401332d38cd4d981245de618000
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2017-02-23 11:38:56 +0545

    use semantic ui and remove sanitize css base

--- client/index.jsx
+++ client/index.jsx
@@ -4,9 +4,6 @@ import { ApolloProvider } from 'react-apollo'
 import Routes from './routes'
 import { store, client } from './reducers'
 
-// Sanitize CSS - CSS Base
-import '../node_modules/sanitize.css/sanitize.css'
-
 // Hot Reload
 if (module.hot) module.hot.accept()
 
