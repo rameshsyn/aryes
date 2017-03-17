fbf44979f54a66c16b653655c062070fbdae0219
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2017-03-17 19:55:45 +0545

    design basic ui of session profile

--- client/components/management/session/profile_session.js
+++ client/components/management/session/profile_session.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component, PropTypes } from 'react'
 import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
@@ -13,13 +14,65 @@ import {
   Loader,
   Segment,
   Header,
+  Search,
   Container,
   Label
 } from 'semantic-ui-react'
 
+const source = [
+  {title: 'ramesh'},
+  {title: 'basanta'},
+  {title: 'pramish'},
+  {title: 'suman'}
+]
 class Student extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      isLoading: false,
+      results: [],
+      value: ''
+    }
+  }
+  componentWillMount () {
+    this.resetComponent.bind(this)
+  }
+  resetComponent () {
+    this.setState({ isLoading: false, results: [], value: '' })
+  }
+  handleResultSelect (e, result) {
+    this.setState({ value: result.title })
+  }
+  handleSearchChange (e, value) {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.resetComponent.bind(this)
+
+      // const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      // const isMatch = (result) => re.test(result.title)
+
+      // const filteredResults = _.reduce(source, (memo, data, name) => {
+      //   const results = _.filter(data.results, isMatch)
+
+      //   if (results.length) {
+      //     memo[name] = { name, results }
+      //   }
+
+      //   return memo
+      // }, {})
+      const filteredResults = source.filter((t, i) => {
+        if (t.title.indexOf(this.state.value) !== -1) {
+          return true
+        }
+      })
+      // console.log(filteredResults)
+
+      this.setState({
+        isLoading: false,
+        results: filteredResults
+      })
+    }, 500)
   }
   render () {
     if (this.props.data.loading) {
@@ -29,6 +82,8 @@ class Student extends Component {
         </Dimmer>
       )
     }
+    const { isLoading, value, results } = this.state
+
     return (
       <Container>
         <br />
@@ -39,7 +94,13 @@ class Student extends Component {
                 <Header as='h1' textAlign='center'>{this.props.data.session[0].name}</Header>
               </Grid.Column>
               <Grid.Column>
-                <Button color='green' floated='right'>Add Student</Button>
+                  <Search
+                    loading={isLoading}
+                    onResultSelect={this.handleResultSelect.bind(this)}
+                    onSearchChange={this.handleSearchChange.bind(this)}
+                    results={results}
+                    value={value}
+                  />
               </Grid.Column>
             </Grid.Row>
           </Grid>
